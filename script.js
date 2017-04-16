c262a22365d96a9d05e07bf22688f3aaa3dd9999
gffrancheska <gffrancheska@gmail.com>
2017-04-16 17:13:07 -0400

    Adding comments.

--- script.js
+++ script.js
@@ -82,22 +82,22 @@ class game {
 
 			case 1: /* Token B & Column B */
 				for (let b = 36; b >= 1; b -= 7){ // Checking from the bottom to the top of this.board array. (36, 29, 22, 15, 8 and 1)
-					if (this.board[b] === null) {
+					if (this.board[b] === null) { // If available space.
 						for (let n = 11; n >= 6; n -= 1) { // Checking from the bottom to the top of one column.
-							if (document.getElementById(n).style.backgroundColor === '') {
-								let soundEffect = new Audio ('audio/token.mp3');
-								soundEffect.play();
-									if ((this.currentPlayer === 'Player 1') && (this.end === false)) {
+							if (document.getElementById(n).style.backgroundColor === '') { // If no background color (equals to available space).
+								let soundEffect = new Audio ('audio/token.mp3'); // Adding sound effect.
+								soundEffect.play(); // Calling the sound effect.
+									if ((this.currentPlayer === 'Player 1') && (this.end === false)) { // If current player is player 1 and game is in progress.
 									document.getElementById(n).style.background = 'radial-gradient(#000000, #0C0C0C, #383838)'; // Black Gradient	
 									document.getElementById(n).style.backgroundColor = '#000000'; // Black
-									this.board[b] = this.color;
-									this.result();
+									this.board[b] = this.color; // In this.board array, in the position b, add the current player's color (Black).
+									this.result(); // Calling the function to check result.
 									}
-									else if ((this.currentPlayer === 'Player 2') && (this.end === false)) {
+									else if ((this.currentPlayer === 'Player 2') && (this.end === false)) { // If current player is player 2 and game is in progress.
 									document.getElementById(n).style.background = 'radial-gradient(#720303, #720303, #AA0808)'; // Red Gradient	
 									document.getElementById(n).style.backgroundColor = '#720303'; // Red
-									this.board[b] = this.color;
-									this.result();
+									this.board[b] = this.color; // In this.board array, in the position b, add the current player's color (Red).
+									this.result(); // Calling the function to check result.
 									};	
 								return; 
 				};	};	};	};
@@ -108,23 +108,23 @@ class game {
 			break;
 
 			case 2: /* Token C & Column C */
-				for (let b = 37; b >= 2; b -= 7){ // this.board array
-					if (this.board[b] === null) {
+				for (let b = 37; b >= 2; b -= 7){ // Checking from the bottom to the top of this.board array. (37, 30, 23, 16, 9 and 2)
+					if (this.board[b] === null) { // If available space.
 						for (let n = 17; n >= 12; n -= 1) { // Checking from the bottom to the top of one column.
-							if (document.getElementById(n).style.backgroundColor === '') {
-								let soundEffect = new Audio ('audio/token.mp3');
-								soundEffect.play();
-									if ((this.currentPlayer === 'Player 1') && (this.end === false)) {
+							if (document.getElementById(n).style.backgroundColor === '') { // If no background color (equals to available space).
+								let soundEffect = new Audio ('audio/token.mp3'); // Adding sound effect.
+								soundEffect.play(); // Calling the sound effect.
+									if ((this.currentPlayer === 'Player 1') && (this.end === false)) { // If current player is player 1 and game is in progress.
 									document.getElementById(n).style.background = 'radial-gradient(#000000, #0C0C0C, #383838)'; // Black Gradient
 									document.getElementById(n).style.backgroundColor = '#000000'; // Black
-									this.board[b] = this.color;
-									this.result();
+									this.board[b] = this.color; // In this.board array, in the position b, add the current player's color (Black).
+									this.result(); // Calling the function to check result.
 									}
-									else if ((this.currentPlayer === 'Player 2') && (this.end === false)) {
+									else if ((this.currentPlayer === 'Player 2') && (this.end === false)) { // If current player is player 2 and game is in progress.
 									document.getElementById(n).style.background = 'radial-gradient(#720303, #720303, #AA0808)'; // Red Gradient	
 									document.getElementById(n).style.backgroundColor = '#720303'; // Red
-									this.board[b] = this.color;
-									this.result();
+									this.board[b] = this.color; // In this.board array, in the position b, add the current player's color (Red).
+									this.result(); // Calling the function to check result.
 									};	
 								return; 
 				};	};	};	};
@@ -135,23 +135,23 @@ class game {
 			break;
 
 			case 3: /* Token D & Column D */
-				for (let b = 38; b >= 3; b -= 7){ // this.board array
-					if (this.board[b] === null) {
+				for (let b = 38; b >= 3; b -= 7){ // Checking from the bottom to the top of this.board array. (38, 31, 24, 17, 10 and 3)
+					if (this.board[b] === null) { // If available space.
 						for (let n = 23; n >= 18; n -= 1) { // Checking from the bottom to the top of one column.
-							if (document.getElementById(n).style.backgroundColor === '') {
-								let soundEffect = new Audio ('audio/token.mp3');
-								soundEffect.play();
-									if ((this.currentPlayer === 'Player 1') && (this.end === false)) {
+							if (document.getElementById(n).style.backgroundColor === '') { // If no background color (equals to available space).
+								let soundEffect = new Audio ('audio/token.mp3'); // Adding sound effect.
+								soundEffect.play(); // Calling the sound effect.
+									if ((this.currentPlayer === 'Player 1') && (this.end === false)) { // If current player is player 1 and game is in progress.
 									document.getElementById(n).style.background = 'radial-gradient(#000000, #0C0C0C, #383838)'; // Black Gradient	
 									document.getElementById(n).style.backgroundColor = '#000000'; // Black
-									this.board[b] = this.color;
-									this.result();
+									this.board[b] = this.color; // In this.board array, in the position b, add the current player's color (Black).
+									this.result(); // Calling the function to check result.
 									}
-									else if ((this.currentPlayer === 'Player 2') && (this.end === false)) {
+									else if ((this.currentPlayer === 'Player 2') && (this.end === false)) { // If current player is player 2 and game is in progress.
 									document.getElementById(n).style.background = 'radial-gradient(#720303, #720303, #AA0808)'; // Red Gradient	
 									document.getElementById(n).style.backgroundColor = '#720303'; // Red
-									this.board[b] = this.color;
-									this.result();
+									this.board[b] = this.color; // In this.board array, in the position b, add the current player's color (Red).
+									this.result(); // Calling the function to check result.
 									};	
 								return; 
 				};	};	};	};
@@ -162,23 +162,23 @@ class game {
 			break;
 
 			case 4: /* Token E & Column E */
-				for (let b = 39; b >= 4; b -= 7){ // this.board array
-					if (this.board[b] === null) {
+				for (let b = 39; b >= 4; b -= 7){ // Checking from the bottom to the top of this.board array. (39, 32, 25, 18, 11 and 4)
+					if (this.board[b] === null) { // If available space.
 						for (let n = 29; n >= 24; n -= 1) { // Checking from the bottom to the top of one column.
-							if (document.getElementById(n).style.backgroundColor === '') {
-								let soundEffect = new Audio ('audio/token.mp3');
-								soundEffect.play();
-									if ((this.currentPlayer === 'Player 1') && (this.end === false)) {
+							if (document.getElementById(n).style.backgroundColor === '') { // If no background color (equals to available space).
+								let soundEffect = new Audio ('audio/token.mp3'); // Adding sound effect.
+								soundEffect.play(); // Calling the sound effect.
+									if ((this.currentPlayer === 'Player 1') && (this.end === false)) { // If current player is player 1 and game is in progress.
 									document.getElementById(n).style.background = 'radial-gradient(#000000, #0C0C0C, #383838)'; // Black Gradient	
 									document.getElementById(n).style.backgroundColor = '#000000'; // Black
-									this.board[b] = this.color;
-									this.result();
+									this.board[b] = this.color; // In this.board array, in the position b, add the current player's color (Black).
+									this.result(); // Calling the function to check result.
 									}
-									else if ((this.currentPlayer === 'Player 2') && (this.end === false)) {
+									else if ((this.currentPlayer === 'Player 2') && (this.end === false)) { // If current player is player 2 and game is in progress.
 									document.getElementById(n).style.background = 'radial-gradient(#720303, #720303, #AA0808)'; // Red Gradient	
 									document.getElementById(n).style.backgroundColor = '#720303'; // Red
-									this.board[b] = this.color;
-									this.result();
+									this.board[b] = this.color; // In this.board array, in the position b, add the current player's color (Red).
+									this.result(); // Calling the function to check result.
 									};	
 								return; 
 				};	};	};	};
@@ -189,23 +189,23 @@ class game {
 			break;
 
 			case 5:/* Token F & Column F */
-				for (let b = 40; b >= 5; b -= 7){ // this.board array
-					if (this.board[b] === null) {
+				for (let b = 40; b >= 5; b -= 7){ // Checking from the bottom to the top of this.board array. (40, 33, 26, 19, 12 and 5)
+					if (this.board[b] === null) { // If available space.
 						for (let n = 35; n >= 30; n -= 1) { // Checking from the bottom to the top of one column.
-							if (document.getElementById(n).style.backgroundColor === '') {
-								let soundEffect = new Audio ('audio/token.mp3');
-								soundEffect.play();
-									if ((this.currentPlayer === 'Player 1') && (this.end === false)) {
+							if (document.getElementById(n).style.backgroundColor === '') { // If no background color (equals to available space).
+								let soundEffect = new Audio ('audio/token.mp3'); // Adding sound effect.
+								soundEffect.play(); // Calling the sound effect.
+									if ((this.currentPlayer === 'Player 1') && (this.end === false)) { // If current player is player 1 and game is in progress.
 									document.getElementById(n).style.background = 'radial-gradient(#000000, #0C0C0C, #383838)'; // Black Gradient	
 									document.getElementById(n).style.backgroundColor = '#000000'; // Black
-									this.board[b] = this.color;
-									this.result();
+									this.board[b] = this.color; // In this.board array, in the position b, add the current player's color (Black).
+									this.result(); // Calling the function to check result.
 									}
-									else if ((this.currentPlayer === 'Player 2') && (this.end === false)) {
+									else if ((this.currentPlayer === 'Player 2') && (this.end === false)) { // If current player is player 2 and game is in progress.
 									document.getElementById(n).style.background = 'radial-gradient(#720303, #720303, #AA0808)'; // Red Gradient	
 									document.getElementById(n).style.backgroundColor = '#720303'; // Red
-									this.board[b] = this.color;
-									this.result();
+									this.board[b] = this.color; // In this.board array, in the position b, add the current player's color (Red).
+									this.result(); // Calling the function to check result.
 									};	
 								return; 
 				};	};	};	};
@@ -216,27 +216,27 @@ class game {
 			break;
 
 			case 6:/* Token G & Column G */
-				for (let b = 41; b >= 6; b -= 7){ // this.board array
-					if (this.board[b] === null) {
+				for (let b = 41; b >= 6; b -= 7){ // Checking from the bottom to the top of this.board array. (41-last position in array-, 34, 27, 20, 13 and 6)
+					if (this.board[b] === null) { // If available space.
 						for (let n = 41; n >= 36; n -= 1) { // Checking from the bottom to the top of one column.
-							if (document.getElementById(n).style.backgroundColor === '') {
-								let soundEffect = new Audio ('audio/token.mp3');
-								soundEffect.play();
-									if ((this.currentPlayer === 'Player 1') && (this.end === false)) {
+							if (document.getElementById(n).style.backgroundColor === '') { // If no background color (equals to available space).
+								let soundEffect = new Audio ('audio/token.mp3'); // Adding sound effect.
+								soundEffect.play(); // Calling the sound effect.
+									if ((this.currentPlayer === 'Player 1') && (this.end === false)) { // If current player is player 1 and game is in progress.
 									document.getElementById(n).style.background = 'radial-gradient(#000000, #0C0C0C, #383838)'; // Black Gradient	
 									document.getElementById(n).style.backgroundColor = '#000000'; // Black
-									this.board[b] = this.color;
-									this.result();
+									this.board[b] = this.color; // In this.board array, in the position b, add the current player's color (Black).
+									this.result(); // Calling the function to check result.
 									}
-									else if ((this.currentPlayer === 'Player 2') && (this.end === false)) {
+									else if ((this.currentPlayer === 'Player 2') && (this.end === false)) { // If current player is player 2 and game is in progress.
 									document.getElementById(n).style.background = 'radial-gradient(#720303, #720303, #AA0808)'; // Red Gradient		
 									document.getElementById(n).style.backgroundColor = '#720303'; // Red
-									this.board[b] = this.color;
-									this.result();
+									this.board[b] = this.color; // In this.board array, in the position b, add the current player's color (Red).
+									this.result(); // Calling the function to check result.
 									};	
 								return; 
 				};	};	};	};
-				// User can't insert a token, if the column G is full.
+				// With this statement, user can't insert a token, if the column G is full.
 				if (this.board[6] !== null) {
 				token[6].removeEventListener('click', move);
 				};
@@ -245,11 +245,7 @@ class game {
 			default:
 			console.log('Not a token.');
 			break;
-			};
-		this.result();
-		};
-	  };
-	};
+	};	};	};	};
 
 /*                          */
 /*          Result          */
@@ -568,16 +564,16 @@ class game {
 		&& ((this.board[15] === this.color) && (this.board[21])	=== this.color))) { 
 		this.end = true;
 		document.getElementById('drop').style.display = 'none'; // Tokens in the top of the grid will be hidden, so players can't click.
-		document.getElementById('currentP').style.display = 'none';
-		document.getElementById('message').style.display = 'block';
+		document.getElementById('currentP').style.display = 'none'; // Hide the current player message.
+		document.getElementById('message').style.display = 'block'; // Turn from none to block, the id where I want to display the winner message.
 		document.getElementById('message').innerHTML = "Congratulations " + this.currentPlayer + " (" + this.color + "), you won!"; // Display in the browser the result.
-		console.log("Congratulations " + this.currentPlayer + " (" + this.color + "), you won!");
+		console.log("Congratulations " + this.currentPlayer + " (" + this.color + "), you won!"); 
 	}
 	else if (this.count === 42) {
 		this.end = true;
 		document.getElementById('drop').style.display = 'none'; // Tokens in the top of the grid will be hidden, so players can't click.
-		document.getElementById('currentP').style.display = 'none';
-		document.getElementById('message').style.display = 'block';
+		document.getElementById('currentP').style.display = 'none'; // Hide the current player message.
+		document.getElementById('message').style.display = 'block'; // Turn from none to block, the id where I want to display that the game end in a tie.
 		document.getElementById('message').innerHTML = "It's a tie!"; // Display in the browser the result.
 		console.log("It's a tie!");
 	}
@@ -592,12 +588,12 @@ class game {
 
 	switchPlayer() {
 		switch(this.currentPlayer) {
-			case 'Player 1': // When player is 1, change to player 2.
+			case 'Player 1': // When player is 1, change to player 2 and change the color of the clickeable tokens.
 				this.currentPlayer = 'Player 2';
 				this.color = 'Red';
 				document.getElementById('player').innerHTML = 'Player 2 (Red!)';
 				document.getElementById('player').style.color = '#AA0808';
-				document.getElementById('a').style.backgroundColor = '#AA0808';
+				document.getElementById('a').style.backgroundColor = '#AA0808'; // In case that the browser does not support gradient.
 				document.getElementById('b').style.backgroundColor = '#AA0808';
 				document.getElementById('c').style.backgroundColor = '#AA0808';
 				document.getElementById('d').style.backgroundColor = '#AA0808';
@@ -614,12 +610,12 @@ class game {
 				console.log('Next turn: ' + this.currentPlayer + ' (Red!)');
 				break;
 
-			case 'Player 2': // When player is 2, change to player 1.
+			case 'Player 2': // When player is 2, change to player 1 and change the color of the clickeable tokens.
 				this.currentPlayer = 'Player 1';
 				this.color = 'Black';
 				document.getElementById('player').innerHTML = 'Player 1 (Black!)';
 				document.getElementById('player').style.color = '#000000';
-				document.getElementById('a').style.backgroundColor = '#000000';
+				document.getElementById('a').style.backgroundColor = '#000000'; // In case that the browser does not support gradient.
 				document.getElementById('b').style.backgroundColor = '#000000';
 				document.getElementById('c').style.backgroundColor = '#000000';
 				document.getElementById('d').style.backgroundColor = '#000000';
@@ -650,8 +646,8 @@ class game {
 		let ConnectFour = new game;
 		ConnectFour.init(); // Initialization of the game.
 
-		let move = function() {
-			ConnectFour.moveToken();
+		let move = function() { // For the event listener.
+			ConnectFour.moveToken(); 
 		};
 
 		// Adding event listener to tokens.
